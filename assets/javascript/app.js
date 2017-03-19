6f2b30179abe32cca50960ba77bbd30471958cac
Alek Shnayder <alek.shnayder@gmail.com>
2017-03-19 12:17:41 -0500

    Use https for api url instead of http

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -96,7 +96,7 @@ $(document).ready(function(){
 
 	//request URLS from Giphy API
 	function getGifs(search){
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         search + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({
